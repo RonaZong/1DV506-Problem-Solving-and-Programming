c317a4d516aab873381fd546534478dd495e214e
Xingrong Zong <xz222bb@student.lnu.se>
2021-12-03 02:19:49 +0100

    2012.12.3

--- 1DV506/src/xz222bb_assign3/MarsMain.java
+++
@@ -1,8 +0,0 @@
-package xz222bb_assign3;
-
-public class MarsMain {
-    public static void main(String[] args) {
-        Planet mars = new Planet("Mars", 4, 2, 1.666, 1.382);
-        mars.print();
-    }
-}
--- 1DV506/src/xz222bb_assign3/MoonMain.java
+++
@@ -1,8 +0,0 @@
-package xz222bb_assign3;
-
-public class MoonMain {
-    public static void main(String[] args) {
-        Moon ganymede = new Moon("Ganymede", 5262);
-        ganymede.print();
-    }
-}
